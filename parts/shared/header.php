751081644c76ed08b6fe4d7b68a49d93b49c6746
Brian Siklinski <bszyk.codes@gmail.com>
2022-07-03 09:45:54 -0500

    header mobile styling

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -34,28 +34,29 @@ if ( $post_query->have_posts() ) :
 endif;
 ?>
 
-<nav class="navbar navbar-expand-sm navbar-light bg-light" style="background-image:url(<?php echo $header_image; ?>);background-size:cover;background-position:center;box-shadow:inset 0 0 0 2000px rgba(<?php echo $h_color['red']; ?>, <?php echo $h_color['green']; ?>, <?php echo $h_color['blue']; ?>, 0.9);">
+<nav class="navbar navbar-expand navbar-light bg-light" style="background-image:url(<?php echo $header_image; ?>);background-size:cover;background-position:center;box-shadow:inset 0 0 0 2000px rgba(<?php echo $h_color['red']; ?>, <?php echo $h_color['green']; ?>, <?php echo $h_color['blue']; ?>, 0.9);">
 	<div class="container-fluid">
-		<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#primaryNav"
+		<!-- <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#primaryNav"
 			aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
-		</button>
+		</button> -->
 		<div class="offcanvas offcanvas-start" tabindex="-1" id="primaryNav">
-			<div class="offcanvas-header">
+			<!-- <div class="offcanvas-header">
 				<h5 class="offcanvas-title" id="offcanvasNavbarLabel"><?php echo get_bloginfo( 'name' ); ?></h5>
 				<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
 					aria-label="Close"></button>
-			</div>
-			<div class="offcanvas-body mx-4 py-3">
-				<div class="col-6 d-flex flex-column justify-content-center">
-					<div class="">
-						<a class="navbar-brand d-none d-sm-inline-flex" href="<?php echo get_site_url(); ?>/">
+			</div> -->
+			<div class="offcanvas-body mx-4 py-3 flex-column flex-sm-row align-items-center">
+				<div class="col-12 col-sm-6 d-flex flex-column">
+					<div class="row text-center text-sm-start">
+						<a class="navbar-brand" href="<?php echo get_site_url(); ?>/">
 							<h1><?php echo get_bloginfo( 'name' ); ?></h1>
 						</a>
-					</div>
+					
 					<div class=""><small><?php echo get_bloginfo( 'description' ); ?></small></div>
+					</div>
 				</div>
-				<div class="col-6 d-flex flex-column align-items-end justify-content-center">
+				<div class="col-12 col-sm-6 d-flex flex-column align-items-center align-items-sm-end justify-content-center text-center text-sm-start mt-4 mt-sm-0">
 					<ul class="list-unstyled m-0">
 						<li>🚴 <?php echo $total_riding_days; ?> Riding Days</li>
 						<li>🛣 <?php echo $total_miles; ?> Miles</li>
@@ -89,6 +90,7 @@ endif;
 				);
 				?>
 				 -->
+
 			</div>
 		</div>
 	</div>
