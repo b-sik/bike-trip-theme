3b681b7f156721427f3e94e144b32ec85108098e
Brian Siklinski <brian.siklinski@19ideas.com>
2022-08-24 15:44:31 -0400

    style pagination nav items

--- js/btt-pagination.js
+++ js/btt-pagination.js
@@ -9,18 +9,28 @@ export class BTT_Pagination {
   }
 
   init() {
+    const navStyles = ['w-100', 'd-flex', 'position-relative'];
+
     if (this.prev && this.next) {
+      navStyles.push('justify-content-between');
       this.nav.insertBefore(this.newDiv(), this.next);
+      this.nav.classList.add(...navStyles);
     } else if (this.prev) {
       this.nav.append(this.newDiv());
+      this.nav.classList.add(...navStyles);
     } else if (this.next) {
+      navStyles.push('justify-content-end');
       this.nav.prepend(this.newDiv());
+      this.nav.classList.add(...navStyles);
     }
   };
 
   newDiv = () => {
     const newDiv = document.createElement('div');
     newDiv.classList.add('pagination-numbers');
+    newDiv.style.position = 'absolute';
+    newDiv.style.left = "50%";
+    newDiv.style.transform = 'translateX(-50%)';
 
     this.numbers.forEach((num) => {
       newDiv.append(num);
