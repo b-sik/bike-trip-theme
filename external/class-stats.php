875bcb2b26f250f28c49410be0d4aa856112e0ce
Brian Siklinski <bszyk.codes@gmail.com>
2022-08-27 18:37:25 -0400

    check for last page instead of file count for stats

--- external/class-stats.php
+++ external/class-stats.php
@@ -34,13 +34,12 @@ class Stats {
 	 *
 	 * @var string Last file count meta key.
 	 */
-	public $last_file_count_meta_key = '__gpx_stats_file_count';
+	public $last_post_meta_key = '__gpx_stats_last_post';
 
 	/**
 	 * Construct.
 	 */
 	public function __construct() {
-		$this->osm            = new OSM_Custom();
 		$this->config_page_id = BTT_Utilities::get_config_post_id();
 	}
 
@@ -50,35 +49,47 @@ class Stats {
 	 * @return array
 	 */
 	public function total_stats() {
-		$gpx_file_count = $this->osm->get_gpx_file_count();
+		$last_post_id = $this->get_last_post_id();
 
-		if ( ! metadata_exists( 'post', $this->config_page_id, $this->last_file_count_meta_key ) ) {
-			update_post_meta( $this->config_page_id, $this->last_file_count_meta_key, $gpx_file_count );
+		if ( ! metadata_exists( 'post', $this->config_page_id, $this->last_post_meta_key ) ) {
+			update_post_meta( $this->config_page_id, $this->last_post_meta_key, $last_post_id );
 			$this->total_stats();
 		}
 
-		$last_file_count_meta = (int) get_post_meta( $this->config_page_id, $this->last_file_count_meta_key, true );
+		$last_post_meta = (int) get_post_meta( $this->config_page_id, $this->last_post_meta_key, true );
 
-		if ( $gpx_file_count === $last_file_count_meta ) {
+		if ( $last_post_id === $last_post_meta ) {
 			if ( metadata_exists( 'post', $this->config_page_id, $this->stats_meta_key ) ) {
 				return get_post_meta( $this->config_page_id, $this->stats_meta_key, true );
 			} else {
-				return $this->total_stats_do_loop( $this->config_page_id, $gpx_file_count );
+				return $this->total_stats_do_loop( $this->config_page_id, $last_post_id );
 			}
 		} else {
-			return $this->total_stats_do_loop( $this->config_page_id, $gpx_file_count );
+			return $this->total_stats_do_loop( $this->config_page_id, $last_post_id );
 		}
+	}
+
+	public function get_last_post_id() {
+		global $wpdb;
+
+		$query = "SELECT ID FROM $wpdb->posts ORDER BY ID DESC LIMIT 0,1";
+
+		$result = $wpdb->get_results( $query ); // phpcs:ignore
+		$row    = $result[0];
+		$id     = $row->ID;
+
+		return (int) $id;
 	}
 
 	/**
 	 * Do the posts loop to get fresh stats.
 	 *
 	 * @param int $page_id Page ID to attach meta.
-	 * @param int $gpx_file_count Count of files in GPX upload folder.
+	 * @param int $last_post_id Count of files in GPX upload folder.
 	 *
 	 * @return array
 	 */
-	public function total_stats_do_loop( $page_id, $gpx_file_count ) {
+	public function total_stats_do_loop( $page_id, $last_post_id ) {
 		$riding_days = 0;
 		$miles       = 0;
 		$elev_gain   = 0;
@@ -114,7 +125,7 @@ class Stats {
 		);
 
 		update_post_meta( $page_id, $this->stats_meta_key, $final_stats );
-		update_post_meta( $page_id, $this->last_file_count_meta_key, $gpx_file_count );
+		update_post_meta( $page_id, $this->last_post_meta_key, $last_post_id );
 
 		return $final_stats;
 	}
